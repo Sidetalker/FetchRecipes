ce3bc8ddd9119e2a21ddc2f99a02abd74e0ade7f
Kevin Sullivan <kevin@sideapps.com>
2025-01-18 10:39:36 -0500

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Kevin Sullivan on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FetchRecipesTests/FetchRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesTests.swift
+//  FetchRecipesTests
+//
+//  Created by Kevin Sullivan on 1/18/25.
+//
+
+import Testing
+@testable import FetchRecipes
+
+struct FetchRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
